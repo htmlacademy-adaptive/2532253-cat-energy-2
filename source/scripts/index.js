f1d7d763263ae220fbd4dfe6f6923d5cb3488b28
LailaAlba <z1001@yandex.ru>
2024-12-15 14:42:44 +0300

    верстка планшет

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,24 @@
 const sliderControl = document.querySelector('.slider__control');
 const slider = document.querySelector('.slider');
+const nav = document.querySelector('.main-nav');
+const toggleButton = document.querySelector('.main-nav__toggle');
 
-let flag = false;
-sliderControl.addEventListener('click', () => {
-  if (!flag) {
-    slider.classList.add('slider--after');
-    flag = true;
-  }else{
-    slider.classList.toggle('slider--after');
-    slider.classList.toggle('slider--before');
-  }
+nav.classList.add('main-nav--closed');
+
+if (slider){
+  let flag = false;
+  sliderControl.addEventListener('click', () => {
+    if (!flag) {
+      slider.classList.add('slider--after');
+      flag = true;
+    }else{
+      slider.classList.toggle('slider--after');
+      slider.classList.toggle('slider--before');
+    }
+  });
+}
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('main-nav--closed');
+  nav.classList.toggle('main-nav--opened');
 });
